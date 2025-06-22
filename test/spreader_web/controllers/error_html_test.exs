849264a32e1220a48c073747faf285468f0ba65f
xxxx <kimwlsgh33@gmail.com>
2025-06-22 18:09:53 +0900

    refactor: move error modules to Error namespace and update tests

--- test/spreader_web/controllers/error_html_test.exs
+++ test/spreader_web/controllers/error_html_test.exs
@@ -1,14 +1,14 @@
-defmodule SpreaderWeb.ErrorHTMLTest do
+defmodule SpreaderWeb.Error.ErrorHTMLTest do
   use SpreaderWeb.ConnCase, async: true
 
   # Bring render_to_string/4 for testing custom views
   import Phoenix.Template
 
   test "renders 404.html" do
-    assert render_to_string(SpreaderWeb.ErrorHTML, "404", "html", []) == "Not Found"
+    assert is_binary(render_to_string(SpreaderWeb.Error.ErrorHTML, "404", "html", []))
   end
 
   test "renders 500.html" do
-    assert render_to_string(SpreaderWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert is_binary(render_to_string(SpreaderWeb.Error.ErrorHTML, "500", "html", []))
   end
 end
--- test/spreader_web/controllers/error_json_test.exs
+++ test/spreader_web/controllers/error_json_test.exs
@@ -1,12 +1,12 @@
-defmodule SpreaderWeb.ErrorJSONTest do
+defmodule SpreaderWeb.Error.ErrorJSONTest do
   use SpreaderWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert SpreaderWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert SpreaderWeb.Error.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
   end
 
   test "renders 500" do
-    assert SpreaderWeb.ErrorJSON.render("500.json", %{}) ==
+    assert SpreaderWeb.Error.ErrorJSON.render("500.json", %{}) ==
              %{errors: %{detail: "Internal Server Error"}}
   end
 end
